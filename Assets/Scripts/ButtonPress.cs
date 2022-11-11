6251868b8f66819507750be944d7ea8e486d483a
Dieu_John <70336903+DieuJohn@users.noreply.github.com>
2022-11-10 17:51:40 -0800

    Interactable Prototype added, press e to interact.

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : Interactable
+{
+    private bool Interacted = false;
+    public Sprite interacted;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player" && Input.GetKey(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    protected virtual void Interact()
+    {
+        if (Interacted == false)
+        {
+            Interacted = true;
+            GetComponent<SpriteRenderer>().sprite = interacted;
+            Debug.Log("Interacted with " + name);
+        }
+    }
+}
